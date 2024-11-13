3952af5a7f69f6419d8d8d2fb426647d8cbde9fd
woziji1 <91675585+woziji1@users.noreply.github.com>
2024-11-13 21:04:18 +0800

    Update soushuba.py

--- soushuba.py
+++ soushuba.py
@@ -69,9 +69,9 @@ class SouShuBaClient:
             "Host": f"{ hostname }",
             "Connection": "keep-alive",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-            "Accept-Language": "zh-CN,cn;q=0.9",
-            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+            "Content-Type": "text/html; charset=gbk",
         }
         self.proxies = proxies
 
